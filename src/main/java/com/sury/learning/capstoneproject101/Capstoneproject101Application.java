85e05a2872ee98f839b729e13a151ea778e9c6d0
Sanskar Suryavanshi personal <ssuryavanshi0001@gmail.com>
2022-12-07 14:20:45 +0530

    day1

---
+++ src/main/java/com/sury/learning/capstoneproject101/Capstoneproject101Application.java
@@ -0,0 +1,13 @@
+package com.sury.learning.capstoneproject101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Capstoneproject101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Capstoneproject101Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sury/learning/capstoneproject101/Capstoneproject101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sury.learning.capstoneproject101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Capstoneproject101ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
